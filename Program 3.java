4d83eb363d3279535572fa24191d54d30e785bc8
Mohit pal <pal001768@gmail.com>
2026-06-15 20:58:08 +0530

    Add Program 3.java with a simple addition program

---
+++ Program 3.java
@@ -0,0 +1,14 @@
+class x
+{
+    static int display(int b,int c)
+    {
+        return b+c;
+    }
+
+    public static void main(String arg[])
+    {
+        int a=5;
+        int d=display(a,6);
+        System.out.println(d);
+    }
+}
