0474156c32d23daedb25ab195aeb27f35f7b76a5
Mohit pal <pal001768@gmail.com>
2026-06-15 20:55:57 +0530

    Create Program 1.java

---
+++ Program 1.java
@@ -0,0 +1,24 @@
+class y
+{
+    static void xyz()
+    {
+        System.out.println("i am xyz");
+    }
+}
+class ACC_open
+{
+    static void lpu()
+    {
+        System.out.println("i am from lpu");
+    }
+    static void abc()
+    {
+        System.out.println("i am in class abc");
+    }
+    public static void main(String arg[])
+    {
+        x.lpu();
+        x.abc();
+        y.xyz();
+    }
+}
