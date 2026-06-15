1a712fc801ea3c2ecfa05efd9feb5dc7830efa9e
Mohit pal <pal001768@gmail.com>
2026-06-15 20:57:21 +0530

    Add Program 2.java with static block and main method

---
+++ Program 2.java
@@ -0,0 +1,12 @@
+class x
+{
+    static
+    {
+        System.out.println("this is static block");
+    }
+
+    public static void main(String arg[])
+    {
+        System.out.println("this is main");
+    }
+}
