d3f7ed497ae536b37482519bf8fd29f29a857bcb
Mohit pal <pal001768@gmail.com>
2026-08-10 11:36:04 +0530

    PrintSubarray

---
+++ PrintSubarray.java
@@ -0,0 +1,18 @@
+public class PrintSubarray {
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 7, 11 };
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i; j < arr.length; j++) {
+
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+
+                System.out.println();
+            }
+        }
+    }
+}
